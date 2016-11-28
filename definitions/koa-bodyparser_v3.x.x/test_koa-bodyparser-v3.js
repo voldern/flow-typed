e36221745353054569df41573b7132c9077ce61d
Espen Volden <espen@volden.me>
2016-11-28 16:50:12 +0100

    Remove koa modules definitions

--- definitions/koa-bodyparser_v3.x.x/test_koa-bodyparser-v3.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import bodyparser from 'koa-bodyparser';
-
-bodyparser();
-// $ExpectError
-bodyparser({
-  strict: true,
-  encode: true,
-});
--- definitions/koa-router_v7.x.x/test_koa-router-v7.js
+++
@@ -1,6 +0,0 @@
-// @flow
-import KoaRouter from 'koa-router';
-
-const A: KoaRouter = new KoaRouter();
-A.get('foo', 'bar', () => {});
-(A.url('foo', {}): string)
